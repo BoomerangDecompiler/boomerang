f8706cd31f1bc009718010a0a54debfbe7d8d54f
emmerik <emmerik>
2002-03-19 07:33:20 +0000

    // 19 Mar 02 - Mike: Added dlopen/dlsym code for opening Unix .so libraries

--- gui/Plugin.cpp
+++ gui/Plugin.cpp
@@ -1,3 +1,4 @@
+// 19 Mar 02 - Mike: Added dlopen/dlsym code for opening Unix .so libraries
 
 #include "stdincs.h"
 
@@ -8,6 +9,16 @@ typedef IPlugin *create_func(void);
 IPlugin *IPlugin::load(wxString filename)
 {
 #if defined(__WXGTK__) || defined(__WXMOTIF__)
+    void* h = dlopen(filename.c_str(), RTLD_LAZY);
+    if (h == NULL) {
+        return NULL;        // No error message or number as yet
+    }
+    create_func* create = (create_func*)dlsym(h, "create");
+    if (create == NULL) {
+        return NULL;
+    }
+    // Problem: we have not closed this library with dlclose, and we
+    // have not stored h anywhere.
 
 #else
 	HMODULE h = LoadLibrary(filename.c_str());
