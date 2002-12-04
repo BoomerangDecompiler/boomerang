cec5b352c96c478818b30e920736cff6ef92ac72
emmerik <emmerik>
2002-12-04 02:56:11 +0000

    Handy program for making sure that operstrings.h is up to date with operator.h

---
+++ db/checkstrings.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include "operstrings.h"
+#include "../include/operator.h"
+
+int main() {
+	int i;
+	if (sizeof(operStrings)/sizeof(char*) == opNumOf) {
+		printf("All is correct\n");
+		return 0;
+	}
+	if ((strcmp(operStrings[opFPlusd],	"opFPlusd") != 0)) {
+		printf("Error before opFPlusd\n"); return 1; }
+	if ((strcmp(operStrings[opSQRTq],	"opSQRTq") != 0)) {
+		printf("Error before opSQRTq\n"); return 1;}
+	if ((strcmp(operStrings[opGtrEqUns],	"opGtrEqUns") != 0)) {
+		printf("Error before opGtrEqUns\n"); return 1;}
+	if ((strcmp(operStrings[opTargetInst],	"opTargetInst") != 0)) {
+		printf("Error before opTargetInst\n"); return 1;}
+	if ((strcmp(operStrings[opList],	"opList") != 0)) {
+		printf("Error before opList\n"); return 1;}
+	if ((strcmp(operStrings[opMachFtr],	"opMachFtr") != 0)) {
+		printf("Error before opMachFtr\n"); return 1;}
+	if ((strcmp(operStrings[opFpop],	"opFpop") != 0)) {
+		printf("Error before opFpop\n"); return 1;}
+	if ((strcmp(operStrings[opExecute],	"opExecute") != 0)) {
+		printf("Error before opExecute\n"); return 1;}
+	if ((strcmp(operStrings[opCodeAddr],	"opCodeAddr") != 0)) {
+		printf("Error before opCodeAddr\n"); return 1;}
+	if ((strcmp(operStrings[opAnull],	"opAnull") != 0)) {
+		printf("Error before opAnull\n"); return 1;}
+	printf("Error near the end\n");
+	return 1;
+}
