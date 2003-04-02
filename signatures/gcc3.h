fde123da14c619eda582bb8608261e99998974e8
emmerik <emmerik>
2003-04-02 05:59:56 +0000

    More sigatures, e.g. for sparc RayTracer

--- signatures/gcc3.h
+++ signatures/gcc3.h
@@ -1,1 +1,5 @@
 void *_Znwj(int bytes);
+void *_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc(void* stream, char* str);
+void* _ZNSolsEm(void* stream, int u);
+void* _ZNSolsEPFRSoS_E(void* stream, void* string);
+void* _ZNSolsEl(void* stream, int l);
