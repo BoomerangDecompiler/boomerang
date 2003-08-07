82ad124043dce47ec0a40b79f4dc6e063bc92e32
emmerik <emmerik>
2003-08-07 12:11:26 +0000

    Added strlen

--- signatures/string.h
+++ signatures/string.h
@@ -2,3 +2,4 @@ int strcmp(char *s1, char *s2);
 int strncmp(char *s1, char *s2, unsigned int n);
 char *strcpy(char *dst, char *src);
 void *memset(void *s, int c, unsigned int n);
+int strlen(char* s);
