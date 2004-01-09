2aa5cd1d213745e4c3cb38b51a8673c9c3b27db7
emmerik <emmerik>
2004-01-09 07:16:47 +0000

    Updated functional tests to actually compile

---
+++ test/source/stattest.c
@@ -0,0 +1,12 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    struct stat st;
+    int res = stat(argv[0], &st);
+    printf("Stat returns %d; size of file is %d\n",
+        res, st.st_size);
+    return res;
+}
+
