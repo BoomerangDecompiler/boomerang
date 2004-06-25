728199373faee1b2ccf5c094146e7e7b7e5a7c2c
emmerik <emmerik>
2004-06-25 05:24:17 +0000

    Improved fromssa2 test

--- test/source/fromssa2.c
+++ test/source/fromssa2.c
@@ -1,14 +1,15 @@
 /* Compile with
 gcc -O4 -o fromssa2 -fno-unroll-loops test/source/fromssa2.c
 */
-void main() {
-int a, x;
-a = 0;
-do {
-  a = a+1;
-  x = a;
-  printf("%d ", a);
-} while (a < 10);
-printf("a is %d, x is %d\n", a, x);
+int main() {
+    int a, x;
+    a = 0;
+    do {
+        a = a+1;
+        x = a;
+        printf("%d ", a);
+    } while (a < 10);
+    printf("a is %d, x is %d\n", a, x);
+    return 0;
 }
 
