f4230817e99c7625035f601fc08bc68f80bf4e80
emmerik <emmerik>
2004-06-29 05:41:23 +0000

    More sparc tests; cleaned up output from functional test

--- functest.sh
+++ functest.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
-# functest.sh functional test script version $Revision$
+# functest.sh functional test script $Revision$
 rm -rf functest
 mkdir functest
 rm -rf functests.out
 ./testOne.sh pentium hello
-./testOne.sh sparc hello
-./testOne.sh pentium  twoproc
-./testOne.sh sparc twoproc
-./testOne.sh pentium  fib
+./testOne.sh sparc   hello
+./testOne.sh pentium twoproc
+./testOne.sh sparc   twoproc
+./testOne.sh pentium fib
+./testOne.sh sparc   fib
 ./testOne.sh pentium fibo-O4 < test/source/fibo-O4.in
-./testOne.sh sparc fibo-O4 < test/source/fibo-O4.in
+./testOne.sh sparc   fibo-O4 < test/source/fibo-O4.in
 ./testOne.sh pentium global1
-./testOne.sh sparc global1
-./testOne.sh pentium  global2
-./testOne.sh sparc global2
-./testOne.sh pentium  global3
-./testOne.sh sparc global3
-./testOne.sh pentium  switch_gcc 2 3 4 5
-./testOne.sh sparc switch_gcc 2 3 4 5
-./testOne.sh pentium  switch_cc 2 3 4
-./testOne.sh sparc switch_cc 2 3 4
+./testOne.sh sparc   global1
+./testOne.sh pentium global2
+./testOne.sh sparc   global2
+./testOne.sh pentium global3
+./testOne.sh sparc   global3
+./testOne.sh pentium switch_gcc 2 3 4 5
+./testOne.sh sparc   switch_gcc 2 3 4 5
+./testOne.sh pentium switch_cc 2 3 4
+./testOne.sh sparc   switch_cc 2 3 4
 ./testOne.sh pentium stattest
-./testOne.sh sparc stattest
+./testOne.sh sparc   stattest
 ./testOne.sh pentium minmax 2 3 4
-./testOne.sh sparc minmax 2 3 4
+./testOne.sh sparc   minmax 2 3 4
 ./testOne.sh pentium minmax2 two
-./testOne.sh sparc minmax2 two
+./testOne.sh sparc   minmax2 two
 ./testOne.sh pentium minmax3 two
 ./testOne.sh pentium printpi
-./testOne.sh sparc printpi
+./testOne.sh sparc   printpi
 ./testOne.sh pentium uns 2 3
-./testOne.sh sparc uns 2 3
+./testOne.sh sparc   uns 2 3
 ./testOne.sh pentium fromssa2
-./testOne.sh sparc fromssa2
+./testOne.sh sparc   fromssa2
 echo === Done ===
--- testOne.sh
+++ testOne.sh
@@ -1,56 +1,62 @@
 #!/bin/bash
-# testOne.sh functional test script version $Revision$
+# testOne.sh functional test script $Revision$
 # Call with test platform, test-program, arguments
 # e.g. "./testOne.sh pentium hello"
 # or   "./testOne.sh sparc fibo 10" 
-echo $*
+echo $* > functest.res
 rm -f functest/$2.c
 ./boomerang -o functest test/$1/$2 2>/dev/null >/dev/null
 ret=$?
 if [[ ret -ge 128 ]]; then
-    echo Boomerang failed with signal $((ret-128)) > functest.res
+    echo Result for $1 $2: Boomerang failed with signal $((ret-128))
+      >> functest.res
 else
     if [[ ! -f functest/$2.c ]]; then
-        echo $1 $2: No boomerang output! > functest.res
+        echo Result for $1 $2: No boomerang output! >> functest.res
     else
         cp functest/$2.c functest.c
         # if test/$1/$2.sed exists, use it to make "known error" corrections
         # to the source code
         if [[ -f test/$1/$2.sed ]]; then
-            echo Warning... $1/$2.sed used
+            echo Warning... $1/$2.sed used >> functest.res
             sed -f test/$1/$2.sed functest.c > functest.tmp
             ret=$?
             if [[ ret -ne 0 ]]; then
-                echo test/$1/$2.sed failed! > functest.res
+                echo test/$1/$2.sed failed! >> functest.res
                 echo
                 exit 10
             fi
             mv functest.tmp functest.c
         fi
-        gcc -o functest.exe functest.c
+        gcc -o functest.exe functest.c >> functest.res 2>&1
         if [[ $? != 0 ]]; then
-            echo $1 $2: Compile failed > functest.res
+            echo Result for $1 $2: Compile failed >> functest.res
         else
             rm -f functest.out
-            ./functest.exe $3 $4 $5 $6 $7 $8 $9 > functest.out
+            ./functest.exe $3 $4 $5 $6 $7 $8 $9 >> functest.out 2>&1
             ret=$?
             if [[ ret -ge 128 ]]; then
-                echo $1 $2: Execution terminated with signal $((ret-128)) >\
-                    functest.res
+                echo Result for $1 $2: Execution terminated with signal \
+                  $((ret-128)) >> functest.res
             else
                 if [[ ret -ne 0 ]]; then
-                    echo Warning! return code from execute was $((ret))
+                    echo Warning! return code from execute was $((ret)) >> \
+                      functest.res
                 fi
-                diff -c test/source/$2.out functest.out
-                if [[ $? != 0 ]]; then
-                    echo $1 $2: failed diff > functest.res
+                diff -c test/source/$2.out functest.out > functest.tmp
+                ret=$?
+                # Filter out control chars that may happen due to bad decomp.
+                tr -d < functest.tmp [:cntrl:] >> functest.res
+                if [[ ret -ne 0 ]]; then
+                    echo Result for $1 $2: failed diff >> functest.res
                 else
-                    echo $1 $2: Passed > functest.res
+                    echo Result for $1 $2: Passed >> functest.res
                 fi
             fi
         fi
     fi
 fi
-cat functest.res
+echo >> functest.res
 cat functest.res >> functests.out
+grep "^Result" functest.res
 echo
