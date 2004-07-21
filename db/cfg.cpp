f39ddd8877ab39a178fa482807754f338c9e5c67
emmerik <emmerik>
2004-07-21 07:27:19 +0000

    oops - polished off the debugging

--- db/cfg.cpp
+++ db/cfg.cpp
@@ -2015,12 +2015,10 @@ void Cfg::dominators() {
         PBB bb = BBs[n];
         std::vector<PBB>::iterator it;
         for (it = bb->m_InEdges.begin(); it != bb->m_InEdges.end(); it++) {
-if (indices.find(*it) == indices.end()) {
-  std::cerr << "BB not in indices: "; (*it)->print(std::cerr);
-  std::list<PBB>::iterator zz;
-  for (zz=m_listBB.begin(); zz != m_listBB.end(); zz++)
-   std::cerr << std::hex << (unsigned)*zz << " "; std::cerr << "\n";
-}
+            if (indices.find(*it) == indices.end()) {
+                std::cerr << "BB not in indices: "; (*it)->print(std::cerr);
+                assert(false);
+            }
             int v = indices[*it];
             int sdash;
             if (dfnum[v] <= dfnum[n])
