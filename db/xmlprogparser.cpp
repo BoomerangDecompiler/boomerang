9ff92795f60a33a657ba2beacd9d2558e52a31e5
emmerik <emmerik>
2004-12-16 10:15:34 +0000

    Kill a warning

--- db/xmlprogparser.cpp
+++ db/xmlprogparser.cpp
@@ -1279,7 +1279,7 @@ void XMLProgParser::addToContext_callstmt(Context *c, int e)
 	}
 	return;
 	}
-	Exp* returnExp;
+	Exp* returnExp = NULL;
 	switch(e) {
 	case e_dest:
 		call->setDest(stack.front()->exp);
