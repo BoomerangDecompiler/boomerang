444b4879696cfeee13424a6d4ebaed9d2554b7b3
emmerik <emmerik>
2005-02-06 01:50:26 +0000

    Added another parameter to testOne.sh to carry a test set digit

--- functest.sh
+++ functest.sh
@@ -2,7 +2,8 @@
 # functest.sh functional test script $Revision$
 # Note: to test with data flow based type analysis, pass a parameter of -Td
 #
-# 02-Feb-05 - Mike: Conditional tests for no type analysis. So all tests should pass whether -Td is passed or not
+# 02 Feb 05 - Mike: Conditional tests for no type analysis. So all tests should pass whether -Td is passed or not
+# 06 Feb 05 - Mike: Pass the test-set parameter to testOne.sh
 #
 if [ -z $1 ]; then BOOMSW="--"; else BOOMSW=$1; fi
 echo Boomerang switch is $BOOMSW
@@ -11,64 +12,71 @@ rm -rf functest
 mkdir functest
 rm -rf functests.out
 
-./testOne.sh pentium hello $BOOMSW
-./testOne.sh sparc   hello $BOOMSW
-./testOne.sh ppc	 hello $BOOMSW
-./testOne.sh pentium twoproc $BOOMSW
-./testOne.sh sparc   twoproc $BOOMSW
-./testOne.sh ppc	 twoproc $BOOMSW
-./testOne.sh pentium fib $BOOMSW
-./testOne.sh sparc   fib $BOOMSW
-./testOne.sh pentium fibo-O4 $BOOMSW < test/source/fibo-O4.in
-./testOne.sh sparc   fibo-O4 $BOOMSW < test/source/fibo-O4.in
-./testOne.sh pentium global1 $BOOMSW
-./testOne.sh sparc   global1 $BOOMSW
-./testOne.sh pentium global2 $BOOMSW
-./testOne.sh sparc   global2 $BOOMSW
-./testOne.sh pentium global3 $BOOMSW
-./testOne.sh sparc   global3 $BOOMSW
+./testOne.sh pentium hello 1 $BOOMSW
+./testOne.sh sparc   hello 1 $BOOMSW
+./testOne.sh ppc	 hello 1 $BOOMSW
+./testOne.sh pentium twoproc 1 $BOOMSW
+./testOne.sh sparc   twoproc 1 $BOOMSW
+./testOne.sh ppc	 twoproc 1 $BOOMSW
+./testOne.sh pentium fib 1 $BOOMSW
+./testOne.sh sparc   fib 1 $BOOMSW
+./testOne.sh pentium fibo-O4 1 $BOOMSW < test/source/fibo-O4.in1
+./testOne.sh sparc   fibo-O4 1 $BOOMSW < test/source/fibo-O4.in1
+./testOne.sh pentium fibo-O4 3 $BOOMSW < test/source/fibo-O4.in3
+./testOne.sh sparc   fibo-O4 3 $BOOMSW < test/source/fibo-O4.in3
+./testOne.sh pentium global1 1 $BOOMSW
+./testOne.sh sparc   global1 1 $BOOMSW
+./testOne.sh pentium global2 1 $BOOMSW
+./testOne.sh sparc   global2 1 $BOOMSW
+./testOne.sh pentium global3 1 $BOOMSW
+./testOne.sh sparc   global3 1 $BOOMSW
 if [ $BOOMSW == "--" ]
 then
   echo Skipping switch tests, requires type analysis
   echo
 else
-./testOne.sh pentium switch_gcc $BOOMSW 2 3 4 5
-./testOne.sh sparc   switch_gcc $BOOMSW 2 3 4 5
-./testOne.sh pentium switch_cc $BOOMSW 2 3 4
-./testOne.sh sparc   switch_cc $BOOMSW 2 3 4
+./testOne.sh pentium switch_gcc 5 $BOOMSW 2 3 4 5
+./testOne.sh sparc   switch_gcc 5 $BOOMSW 2 3 4 5
+./testOne.sh pentium switch_cc 4 $BOOMSW 2 3 4
+./testOne.sh sparc   switch_cc 4 $BOOMSW 2 3 4
+./testOne.sh pentium switch_gcc 1 $BOOMSW
+./testOne.sh sparc   switch_gcc 1 $BOOMSW
+./testOne.sh pentium switch_cc 1 $BOOMSW
+# Below has a known problem, due to setSigArguments (after ind. jump) rewriting arguments that were renamed elsewhere
+#./testOne.sh sparc   switch_cc 1 $BOOMSW
 fi
-./testOne.sh pentium stattest $BOOMSW
-./testOne.sh sparc   stattest $BOOMSW
-./testOne.sh pentium minmax $BOOMSW 2 3 4
-./testOne.sh sparc   minmax $BOOMSW 2 3 4
-./testOne.sh pentium minmax2 $BOOMSW two
-./testOne.sh sparc   minmax2 $BOOMSW two
-./testOne.sh pentium minmax3 $BOOMSW two
+./testOne.sh pentium stattest 1 $BOOMSW
+./testOne.sh sparc   stattest 1 $BOOMSW
+./testOne.sh pentium minmax 1 $BOOMSW 2 3 4
+./testOne.sh sparc   minmax 1 $BOOMSW 2 3 4
+./testOne.sh pentium minmax2 1 $BOOMSW two
+./testOne.sh sparc   minmax2 1 $BOOMSW two
+./testOne.sh pentium minmax3 1 $BOOMSW two
 if [ $BOOMSW == "--" ]
 then
   echo Skipping printpi tests, requires type analysis
   echo
 else
-./testOne.sh pentium printpi $BOOMSW
-./testOne.sh sparc   printpi $BOOMSW
+./testOne.sh pentium printpi 1 $BOOMSW
+./testOne.sh sparc   printpi 1 $BOOMSW
 fi
-./testOne.sh pentium uns $BOOMSW 2 3
-./testOne.sh sparc   uns $BOOMSW 2 3
-./testOne.sh pentium fromssa2 $BOOMSW
-./testOne.sh sparc   fromssa2 $BOOMSW
-./testOne.sh pentium sumarray-O4 $BOOMSW
+./testOne.sh pentium uns 1 $BOOMSW 2 3
+./testOne.sh sparc   uns 1 $BOOMSW 2 3
+./testOne.sh pentium fromssa2 1 $BOOMSW
+./testOne.sh sparc   fromssa2 1 $BOOMSW
+./testOne.sh pentium sumarray-O4 1 $BOOMSW
 if [ $BOOMSW == "--" ]
 then
   echo Skipping sparc sumarray test, requires type analysis
   echo
 else
-./testOne.sh sparc   sumarray-O4 $BOOMSW
+./testOne.sh sparc   sumarray-O4 1 $BOOMSW
 fi
-./testOne.sh pentium bswap $BOOMSW
-./testOne.sh pentium testset -O 2 3 4
-./testOne.sh sparc   andn $BOOMSW
+./testOne.sh pentium bswap 1 $BOOMSW
+./testOne.sh pentium testset 1 -O 2 3 4
+./testOne.sh sparc   andn 1 $BOOMSW
 
-#./testOne.sh pentium line1 $BOOMSW test/source/line1.c
-#./testOne.sh sparc   line1 $BOOMSW test/source/line1.c
+#./testOne.sh pentium line1 1 $BOOMSW test/source/line1.c
+#./testOne.sh sparc   line1 1 $BOOMSW test/source/line1.c
 
 echo === Done ===
--- testOne.sh
+++ testOne.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 # testOne.sh functional test script $Revision$
-# Call with test platform, test-program [,options [,arguments]]
+# Call with test platform, test-program test-set [,option [,arguments]]
+# test-set is a char usually 1-9 for the various .out files, usually use 1 for .out1
 # e.g. "./testOne.sh pentium hello"
-# or   "./testOne.sh sparc fibo -- 10" 
-# Note: at this stage, only one string can be passed as the options, e.g. -O
+# or   "./testOne.sh sparc fibo 1 -- 10" 
+# or   "./testOne.sh sparc switch_cc 6 -Td 2 3 4 5 6"
+# Note: max of 5 parameters passed to the recompiled executable
+# Note: only one string can be passed as the option, e.g. -O or -Td or --
+# $1 = platform $2 = test $3 = test-set $4 = option $5-$9 are parameters to the recompiled executable
+#
+# 06 Feb 05 - Mike: Pass the test-set parameter to testOne.sh
 
 echo $* > functest.res
 rm -f functest/$2.c
-./boomerang -o functest $3 test/$1/$2 2>/dev/null >/dev/null
+./boomerang -o functest $4 test/$1/$2 2>/dev/null >/dev/null
 ret=$?
 if [[ ret -ge 128 ]]; then
-	echo Result for $1 $2: Boomerang FAILED with signal $((ret-128))
+	echo Result for $1 $2: Boomerang FAILED set $3 with signal $((ret-128))
 		>> functest.res
 else
 	if [[ ! -f functest/$2/$2.c ]]; then
-		echo Result for $1 $2: No boomerang output! >> functest.res
+		echo Result for $1 $2: NO BOOMERANG OUTPUT set $3! >> functest.res
 	else
 		cp functest/$2/$2.c functest.c
 		# if test/$1/$2.sed exists, use it to make "known error" corrections to the source code
@@ -34,22 +40,22 @@ else
 			echo Result for $1 $2: Compile FAILED >> functest.res
 		else
 			rm -f functest.out
-			./functest.exe $4 $5 $6 $7 $8 $9 >> functest.out 2>&1
+			./functest.exe $5 $6 $7 $8 $9 >> functest.out 2>&1
 			ret=$?
 			if [[ ret -ge 128 ]]; then
-				echo Result for $1 $2: Execution terminated with signal $((ret-128)) >> functest.res
+				echo Result for $1 $2: EXECUTION TERMINATED with signal $((ret-128)) >> functest.res
 			else
 				if [[ ret -ne 0 ]]; then
 					echo Warning! return code from execute was $((ret)) >> functest.res
 				fi
-				diff -c test/source/$2.out functest.out > functest.tmp
+				diff -c test/source/$2.out$3 functest.out > functest.tmp
 				ret=$?
 				# Filter out control chars that may happen due to bad decomp.
 				tr -d < functest.tmp [:cntrl:] >> functest.res
 				if [[ ret -ne 0 ]]; then
-					echo Result for $1 $2: FAILED diff >> functest.res
+					echo Result for $1 $2: FAILED diff set $3 >> functest.res
 				else
-					echo Result for $1 $2: Passed >> functest.res
+					echo Result for $1 $2: Passed set $3 >> functest.res
 				fi
 			fi
 		fi
