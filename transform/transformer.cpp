02f1489c38883b8e641404f81b38d2702f89dbfc
emmerik <emmerik>
2005-07-04 04:12:10 +0000

    tabified

--- transform/transformer.cpp
+++ transform/transformer.cpp
@@ -2,8 +2,8 @@
  * Copyright (C) 2004, Mike Van Emmerik and Trent Waddington
  */
 /*==============================================================================
- * FILE:       transformer.cpp
- * OVERVIEW:   Implementation of the Transformer and related classes.
+ * FILE:		transformer.cpp
+ * OVERVIEW:	Implementation of the Transformer and related classes.
  *============================================================================*/
 /*
  * $Revision$
@@ -15,10 +15,10 @@
 #pragma warning(disable:4786)
 #endif 
 
-#include <numeric>      // For accumulate
-#include <algorithm>    // For std::max()
-#include <map>          // In decideType()
-#include <sstream>      // Need gcc 3.0 or better
+#include <numeric>			// For accumulate
+#include <algorithm>		// For std::max()
+#include <map>				// In decideType()
+#include <sstream>			// Need gcc 3.0 or better
 #include "types.h"
 #include "statement.h"
 #include "cfg.h"
@@ -36,86 +36,86 @@ std::list<ExpTransformer*> ExpTransformer::transformers;
 
 ExpTransformer::ExpTransformer()
 {
-    transformers.push_back(this);
+	transformers.push_back(this);
 }
 
 std::list<Exp*> cache;
 
 Exp *ExpTransformer::applyAllTo(Exp *p, bool &bMod)
 {
-    for (std::list<Exp*>::iterator it = cache.begin(); it != cache.end(); it++)
-        if (*(*it)->getSubExp1() == *p)
-            return (*it)->getSubExp2()->clone();
+	for (std::list<Exp*>::iterator it = cache.begin(); it != cache.end(); it++)
+		if (*(*it)->getSubExp1() == *p)
+			return (*it)->getSubExp2()->clone();
 
-    Exp *e = p->clone();
-    Exp *subs[3];
-    subs[0] = e->getSubExp1();
-    subs[1] = e->getSubExp2();
-    subs[2] = e->getSubExp3();
+	Exp *e = p->clone();
+	Exp *subs[3];
+	subs[0] = e->getSubExp1();
+	subs[1] = e->getSubExp2();
+	subs[2] = e->getSubExp3();
 
-    for (int i = 0; i < 3; i++)
-        if (subs[i]) {
-            bool mod = false;
-            subs[i] = applyAllTo(subs[i], mod);
-            if (mod && i == 0)
-                e->setSubExp1(subs[i]);
-            if (mod && i == 1)
-                e->setSubExp2(subs[i]);
-            if (mod && i == 2)
-                e->setSubExp3(subs[i]);
-            bMod |= mod;
-//            if (mod) i--;
-        }
+	for (int i = 0; i < 3; i++)
+		if (subs[i]) {
+			bool mod = false;
+			subs[i] = applyAllTo(subs[i], mod);
+			if (mod && i == 0)
+				e->setSubExp1(subs[i]);
+			if (mod && i == 1)
+				e->setSubExp2(subs[i]);
+			if (mod && i == 2)
+				e->setSubExp3(subs[i]);
+			bMod |= mod;
+//			if (mod) i--;
+		}
 
 #if 0
-    LOG << "applyAllTo called on " << e << "\n";
+	LOG << "applyAllTo called on " << e << "\n";
 #endif
-    bool mod;
-    //do {
-        mod = false;
-        for (std::list<ExpTransformer *>::iterator it = transformers.begin(); it != transformers.end(); it++) {
-            e = (*it)->applyTo(e, mod);
-            bMod |= mod;
-        }
-    //} while (mod);
+	bool mod;
+	//do {
+		mod = false;
+		for (std::list<ExpTransformer *>::iterator it = transformers.begin(); it != transformers.end(); it++) {
+			e = (*it)->applyTo(e, mod);
+			bMod |= mod;
+		}
+	//} while (mod);
 
-    cache.push_back(new Binary(opEquals, p->clone(), e->clone()));
-    return e;
+	cache.push_back(new Binary(opEquals, p->clone(), e->clone()));
+	return e;
 }
 
 void ExpTransformer::loadAll()
 {
-    std::string sPath = Boomerang::get()->getProgPath() + "transformations/exp.ts";
+	std::string sPath = Boomerang::get()->getProgPath() + "transformations/exp.ts";
 
-    std::ifstream ifs;
-    ifs.open(sPath.c_str());
+	std::ifstream ifs;
+	ifs.open(sPath.c_str());
 
-    if (!ifs.good()) {
+	if (!ifs.good()) {
 		std::cerr << "can't open `" << sPath.c_str() << "'\n";
-        exit(1);
-    }
+		exit(1);
+	}
 
-    while (!ifs.eof()) {
-        std::string sFile;
-        ifs >> sFile;
-        size_t j = sFile.find('#');
-        if (j != (size_t)-1)
-            sFile = sFile.substr(0, j);
-	    if (sFile.size() > 0 && sFile[sFile.size()-1] == '\n')
-	        sFile = sFile.substr(0, sFile.size()-1);
-        if (sFile == "") continue;
-        std::ifstream ifs1;
-        std::string sPath1 = Boomerang::get()->getProgPath() + "transformations/" + sFile;
-        ifs1.open(sPath1.c_str());
-        if (!ifs1.good()) {
-            LOG << "can't open `" << sPath1.c_str() << "'\n";
-            exit(1);
-        }
-        TransformationParser *p = new TransformationParser(ifs1, false);
-        p->yyparse();
-        ifs1.close();
-    }
-    ifs.close();
+	while (!ifs.eof()) {
+		std::string sFile;
+		ifs >> sFile;
+		size_t j = sFile.find('#');
+		if (j != (size_t)-1)
+			sFile = sFile.substr(0, j);
+		if (sFile.size() > 0 && sFile[sFile.size()-1] == '\n')
+			sFile = sFile.substr(0, sFile.size()-1);
+		if (sFile == "") continue;
+		std::ifstream ifs1;
+		std::string sPath1 = Boomerang::get()->getProgPath() + "transformations/" + sFile;
+		ifs1.open(sPath1.c_str());
+		if (!ifs1.good()) {
+			LOG << "can't open `" << sPath1.c_str() << "'\n";
+			exit(1);
+		}
+		TransformationParser *p = new TransformationParser(ifs1, false);
+		p->yyparse();
+		ifs1.close();
+	}
+	ifs.close();
 }
 
 
