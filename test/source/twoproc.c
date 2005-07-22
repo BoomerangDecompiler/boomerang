a5c7d8f6e54626cc67beb979bc1eb0ec0c92a9a5
emmerik <emmerik>
2005-07-22 05:37:00 +0000

    3+4 -> 11-4 (same result)

--- test/source/twoproc.c
+++ test/source/twoproc.c
@@ -1,10 +1,11 @@
+/* Compile with gcc -O4 -fno-inline -o ...  or cc -xO4 -xinline= -o ...  */
 
 int proc1(int a, int b) {
-    return a + b;
+    return a - b;
 }
 
 int main() {
-    printf("%i\n", proc1(3, 4));
+    printf("%i\n", proc1(11, 4));
 }
 
 
