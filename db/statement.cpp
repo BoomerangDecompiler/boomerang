d2dfda35388efb5f65667aad7b8f71e8c25a69e1
emmerik <emmerik>
2005-12-08 06:40:15 +0000

    Added a test for a definition being an assignment

--- db/statement.cpp
+++ db/statement.cpp
@@ -2239,6 +2239,7 @@ bool CallStatement::ellipsisProcessing(Prog* prog) {
 			for (int i=0; i < n; i++) {
 				def = pa->getStmtAt(i);
 				if (def == NULL) continue;
+				if (!def->isAssign()) continue;
 				Exp* rhs = ((Assign*)def)->getRight();
 				if (rhs == NULL || !rhs->isStrConst()) continue;
 				formatStr = ((Const*)rhs)->getStr();
