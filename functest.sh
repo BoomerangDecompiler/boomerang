ac31260b9db985134bc9a5b28684c1430ed06b6b
emmerik <emmerik>
2006-01-19 10:13:07 +0000

    Gerard Krol's nice improvements in output, and grouping

--- functest.sh
+++ functest.sh
@@ -6,114 +6,124 @@
 # 06 Feb 05 - Mike: Pass the test-set parameter to testOne.sh
 # 24 Dec 05 - Gerard: Support for more than one switch added
 #
-TESTONE=./testOne.sh
-#TESTONE="sh testOne.sh"	# For MinGW (configure me one day)
+
+# On MSYS we need to invoke sh ourselves
+KERNEL=`uname -s`
+if [ ${KERNEL:0:4} = "MSYS" ]
+	then TESTONE="sh testOne.sh"
+	else TESTONE="./testOne.sh"
+fi
+
+# Store the command line switches in BOOMSW
 BOOMSW=$*
+
+# Look for -Td to see if type analysis is enabled
+# If it isn't, we will skip some tests
 TYPEANALYSIS=""
-# look for -Td to see if type analysis is enabled
 while [ "$1" ]; do
 	if [ "$1" = "-Td" ]; then TYPEANALYSIS="YES"; fi
 	shift
 done
-echo Boomerang switches are $BOOMSW
 if [ "$TYPEANALYSIS" ]
 then
-  echo Type analysis enabled
+	echo Type analysis enabled
 else
-  echo "Note: Type analysis is disabled"
-  echo "      To enable type analysis use the \"-Td\" switch"
+	echo "Note: Type analysis is disabled"
+	echo "      To enable type analysis use the \"-Td\" switch"
 fi
-echo
 
+# Clean up
 rm -rf functest
 mkdir functest
 rm -rf functests.out
 
-$TESTONE pentium hello		1 "$BOOMSW"
-$TESTONE sparc   hello		1 "$BOOMSW"
-$TESTONE ppc	 hello		1 "$BOOMSW"
-$TESTONE pentium twoproc	1 "$BOOMSW"
-$TESTONE sparc   twoproc	1 "$BOOMSW"
-$TESTONE ppc	 twoproc	1 "$BOOMSW"
-$TESTONE pentium param1		1 "$BOOMSW"
-$TESTONE sparc   param1		4 "$BOOMSW" '2 3 4'
-$TESTONE pentium restoredparam 3 "$BOOMSW" '2 3'
-$TESTONE pentium fib		1 "$BOOMSW"
-$TESTONE sparc   fib		1 "$BOOMSW"
-$TESTONE pentium fibo-O4	1 "$BOOMSW" < test/source/fibo-O4.in1
-$TESTONE sparc   fibo-O4	1 "$BOOMSW" < test/source/fibo-O4.in1
-$TESTONE pentium fibo-O4	3 "$BOOMSW" < test/source/fibo-O4.in3
-$TESTONE sparc   fibo-O4	3 "$BOOMSW" < test/source/fibo-O4.in3
-$TESTONE pentium global1	1 "$BOOMSW"
-$TESTONE sparc   global1	1 "$BOOMSW"
-$TESTONE pentium global2	1 "$BOOMSW"
-$TESTONE sparc   global2	1 "$BOOMSW"
-$TESTONE pentium global3	1 "$BOOMSW"
-$TESTONE sparc   global3	1 "$BOOMSW"
-if [ ! "$TYPEANALYSIS" ]
-then
-  echo Skipping switch tests, requires type analysis
-  echo
-else
-$TESTONE pentium switch_gcc	5 "$BOOMSW" '2 3 4 5'
-$TESTONE sparc   switch_gcc 5 "$BOOMSW" '2 3 4 5'
-$TESTONE pentium switch_cc	4 "$BOOMSW" '2 3 4'
-$TESTONE sparc   switch_cc	4 "$BOOMSW" '2 3 4'
-$TESTONE pentium switch_gcc	1 "$BOOMSW"
-$TESTONE sparc   switch_gcc	1 "$BOOMSW"
-$TESTONE pentium switch_cc	1 "$BOOMSW"
-$TESTONE sparc   switch_cc	1 "$BOOMSW"
-$TESTONE pentium nestedswitch 4 "$BOOMSW" '2 3 4'
-$TESTONE sparc   nestedswitch 4 "$BOOMSW" '2 3 4'
-fi
-if [ ! "$TYPEANALYSIS" ]
-then
-  echo Skipping stattest, now requires type analysis
-  echo
-else
-  echo Some known failures from here down
-$TESTONE pentium stattest	1 "$BOOMSW"
-$TESTONE sparc   stattest	1 "$BOOMSW"
-fi
-$TESTONE pentium minmax		1 "$BOOMSW" '2 3 4'
-$TESTONE sparc   minmax		1 "$BOOMSW" '2 3 4'
-$TESTONE pentium minmax2	1 "$BOOMSW" 'two'
-$TESTONE sparc   minmax2	1 "$BOOMSW" 'two'
-$TESTONE pentium minmax3	1 "$BOOMSW" 'two'
-if [ ! "$TYPEANALYSIS" ]
-then
-  echo Skipping printpi tests, requires type analysis
-  echo
-else
-$TESTONE pentium printpi	1 "$BOOMSW"
-$TESTONE sparc   printpi	1 "$BOOMSW"
-fi
-$TESTONE pentium uns		1 "$BOOMSW" '2 3'
-$TESTONE sparc   uns		1 "$BOOMSW" '2 3'
-$TESTONE pentium fromssa2	1 "$BOOMSW"
-$TESTONE sparc   fromssa2	1 "$BOOMSW"
-$TESTONE pentium sumarray-O4 1 "$BOOMSW"
-$TESTONE pentium callchain	1 "$BOOMSW"
-$TESTONE sparc	 callchain	1 "$BOOMSW"
-if [ ! "$TYPEANALYSIS" ]
-then
-  echo Skipping sparc sumarray etc tests, require type analysis
-  echo
-else
-$TESTONE sparc   sumarray-O4 1 "$BOOMSW"
-$TESTONE pentium recursion	2 "$BOOMSW" 2
-#$TESTONE sparc   recursion	2 "$BOOMSW" 2
-$TESTONE pentium paramchain	1 "$BOOMSW"
-$TESTONE sparc	 paramchain	1 "$BOOMSW"
-fi
-$TESTONE pentium bswap		1 "$BOOMSW"
-$TESTONE pentium testset	1 -O '2 3 4'
-$TESTONE sparc   andn		1 "$BOOMSW"
+# Run the tests
+echo
+echo === These should be OK ===
+$TESTONE pentium hello			1 "$BOOMSW"
+$TESTONE sparc   hello			1 "$BOOMSW"
+$TESTONE ppc	 hello			1 "$BOOMSW"
+$TESTONE pentium twoproc		1 "$BOOMSW"
+$TESTONE sparc   twoproc		1 "$BOOMSW"
+$TESTONE ppc	 twoproc		1 "$BOOMSW"
+$TESTONE pentium param1			1 "$BOOMSW"
+$TESTONE sparc   param1			4 "$BOOMSW" '2 3 4'
+$TESTONE pentium restoredparam	3 "$BOOMSW" '2 3'
+$TESTONE pentium fib			1 "$BOOMSW"
+$TESTONE sparc   fib			1 "$BOOMSW"
+$TESTONE pentium fibo-O4		1 "$BOOMSW" < test/source/fibo-O4.in1
+$TESTONE sparc   fibo-O4		1 "$BOOMSW" < test/source/fibo-O4.in1
+$TESTONE pentium fibo-O4		3 "$BOOMSW" < test/source/fibo-O4.in3
+$TESTONE sparc   fibo-O4		3 "$BOOMSW" < test/source/fibo-O4.in3
+$TESTONE pentium global1		1 "$BOOMSW"
+$TESTONE sparc   global1		1 "$BOOMSW"
+$TESTONE pentium global2		1 "$BOOMSW"
+$TESTONE sparc   global2		1 "$BOOMSW"
+$TESTONE pentium global3		1 "$BOOMSW"
+$TESTONE sparc   global3		1 "$BOOMSW"
 
-#$TESTONE pentium line1		1 "$BOOMSW" test/source/line1.c
-#$TESTONE sparc   line1		1 "$BOOMSW" test/source/line1.c
+$TESTONE pentium minmax			1 "$BOOMSW" '2 3 4'
+$TESTONE sparc   minmax			1 "$BOOMSW" '2 3 4'
+$TESTONE pentium minmax2		1 "$BOOMSW" 'two'
+$TESTONE sparc   minmax2		1 "$BOOMSW" 'two'
+$TESTONE pentium minmax3		1 "$BOOMSW" 'two'
+$TESTONE pentium uns			1 "$BOOMSW" '2 3'
+$TESTONE sparc   uns			1 "$BOOMSW" '2 3'
+$TESTONE pentium fromssa2		1 "$BOOMSW"
+$TESTONE sparc   fromssa2		1 "$BOOMSW"
+$TESTONE pentium sumarray-O4	1 "$BOOMSW"
+$TESTONE pentium bswap			1 "$BOOMSW"
+$TESTONE pentium testset		1 -O '2 3 4'
+$TESTONE sparc   andn			1 "$BOOMSW"
+$TESTONE pentium callchain		1 "$BOOMSW"
+$TESTONE sparc	 callchain		1 "$BOOMSW"
 
-echo Known failure:
-$TESTONE sparc	elfhashtest	1 "$BOOMSW"
 
+if [ ! "$TYPEANALYSIS" ]
+then
+	echo
+	echo === Skipping tests which require type analysis ===
+else
+	$TESTONE sparc   sumarray-O4	1 "$BOOMSW"
+	$TESTONE pentium paramchain		1 "$BOOMSW"
+	$TESTONE sparc	 paramchain		1 "$BOOMSW"
+
+	#$TESTONE pentium line1			1 "$BOOMSW" test/source/line1.c
+	#$TESTONE sparc   line1			1 "$BOOMSW" test/source/line1.c
+
+	echo
+	echo === Switch tests ===
+	$TESTONE pentium switch_gcc		5 "$BOOMSW" '2 3 4 5'
+	$TESTONE sparc   switch_gcc		5 "$BOOMSW" '2 3 4 5'
+	$TESTONE pentium switch_cc		4 "$BOOMSW" '2 3 4'
+	$TESTONE sparc   switch_cc		4 "$BOOMSW" '2 3 4'
+	$TESTONE pentium switch_gcc		1 "$BOOMSW"
+	$TESTONE sparc   switch_gcc		1 "$BOOMSW"
+	$TESTONE pentium switch_cc		1 "$BOOMSW"
+	$TESTONE sparc   switch_cc		1 "$BOOMSW"
+	$TESTONE pentium nestedswitch	4 "$BOOMSW" '2 3 4'
+	$TESTONE sparc   nestedswitch	4 "$BOOMSW" '2 3 4'
+	
+	echo
+	echo === Double handling tests ===
+	$TESTONE pentium printpi		1 "$BOOMSW"
+	$TESTONE sparc   printpi		1 "$BOOMSW"
+fi
+
+echo
+echo === Issues with the GC ===
+$TESTONE sparc   global2		1 "$BOOMSW"
+$TESTONE sparc   global3		1 "$BOOMSW"
+
+echo
+echo === Known faillures ===
+#$TESTONE sparc   recursion		2 "$BOOMSW" 2
+$TESTONE pentium stattest		1 "$BOOMSW"
+$TESTONE sparc   stattest		1 "$BOOMSW"
+$TESTONE sparc	elfhashtest		1 "$BOOMSW"
+# pentium recursion was working until moderately recently...
+$TESTONE pentium recursion		2 "$BOOMSW" 2
+
+echo
 echo === Done ===
+echo
--- testOne.sh
+++ testOne.sh
@@ -10,17 +10,31 @@
 #
 # 06 Feb 05 - Mike: Pass the test-set parameter to testOne.sh
 # 24 Dec 05 - Gerard: Support for more than one switch added
+# 29 Dec 05 - Gerard: Nicer output
+#
 
 echo $* > functest.res
 rm -f functest/$2/$2.c
-./boomerang -o functest $4 test/$1/$2 2>/dev/null >/dev/null
+
+SPACES="                                                 "
+RES="Result for $1"
+WHITE=${SPACES:0:(18 - ${#RES})}
+RES="$RES$WHITE $2:"
+WHITE=${SPACES:0:(34 - ${#RES})}
+RES=$RES$WHITE
+echo -n -e "$RES"
+
+sh -c "./boomerang -o functest $4 test/$1/$2 2>/dev/null >/dev/null"
 ret=$?
 if [[ ret -ge 128 ]]; then
-	echo Result for $1 $2: Boomerang FAILED set $3 with signal $((ret-128))
-		>> functest.res
+	SIGNAL="signal $((ret-128))"
+	if [ "$SIGNAL" = "signal 9" ]; then SIGNAL="a kill signal"; fi
+	if [ "$SIGNAL" = "signal 11" ]; then SIGNAL="a segmentation fault"; fi
+	if [ "$SIGNAL" = "signal 15" ]; then SIGNAL="a termination signal"; fi
+	RESULT="Boomerang FAILED set $3 with $SIGNAL"
 else
 	if [[ ! -f functest/$2/$2.c ]]; then
-		echo Result for $1 $2: NO BOOMERANG OUTPUT set $3! >> functest.res
+		RESULT="NO BOOMERANG OUTPUT set $3!"
 	else
 		cp functest/$2/$2.c functest.c
 		# if test/$1/$2.sed exists, use it to make "known error" corrections to the source code
@@ -30,20 +44,23 @@ else
 			ret=$?
 			if [[ ret -ne 0 ]]; then
 				echo test/$1/$2.sed FAILED! >> functest.res
-				echo
 				exit 10
 			fi
 			mv functest.tmp functest.c
 		fi
-		gcc -Ioutput -o functest.exe functest.c >> functest.res 2>&1
+		gcc -o functest.exe functest.c >> functest.res 2>&1
 		if [[ $? != 0 ]]; then
-			echo Result for $1 $2: Compile FAILED >> functest.res
+			RESULT="Compile FAILED"
 		else
 			rm -f functest.out
-			./functest.exe $5 >> functest.out 2>&1
+			sh -c "./functest.exe $5 >> functest.out 2>&1"
 			ret=$?
 			if [[ ret -ge 128 ]]; then
-				echo Result for $1 $2: EXECUTION TERMINATED with signal $((ret-128)) >> functest.res
+				SIGNAL="signal $((ret-128))"
+				if [ "$SIGNAL" = "signal 9" ]; then SIGNAL="a kill signal"; fi
+				if [ "$SIGNAL" = "signal 11" ]; then SIGNAL="a segmentation fault"; fi
+				if [ "$SIGNAL" = "signal 15" ]; then SIGNAL="a termination signal"; fi
+				RESULT="EXECUTION TERMINATED with $SIGNAL"
 			else
 				if [[ ret -ne 0 ]]; then
 					echo Warning! return code from execute was $((ret)) >> functest.res
@@ -55,15 +72,18 @@ else
 				# tr -s -d < functest.tmp [:cntrl:] >> functest.res
 				sed -e "s/[[:cntrl:]]//g" functest.tmp >> functest.res
 				if [[ ret -ne 0 ]]; then
-					echo Result for $1 $2: FAILED diff set $3 >> functest.res
+					RESULT="FAILED diff set $3"
 				else
-					echo Result for $1 $2: Passed set $3 >> functest.res
+					RESULT="Passed set $3"
 				fi
 			fi
 		fi
 	fi
 fi
+echo $RESULT
+echo -e "$RES""$RESULT" >> functest.res
 echo >> functest.res
 cat functest.res >> functests.out
-grep "^Result" functest.res
-echo
+#grep "^Result" functest.res
+
+
