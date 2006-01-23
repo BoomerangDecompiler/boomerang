67c5fabe6abd4be042e3a3e6ce4ea2548c91296d
emmerik <emmerik>
2006-01-23 02:01:09 +0000

    Oops - was still not being faithful to the pentium fibo3; now it is

--- test/sparc/fibo3.s
+++ test/sparc/fibo3.s
@@ -1,7 +1,7 @@
 # This version is compiled with gcc version 4.0.0, no optimisation
 # Can't use %o4 and %o5 to save the intermediate result, since the save and restore instructions effectively save and
 # restore them via the %i4 and %i5 registers (since they are not overwritten; a bit like callee save registers).
-# So %i4 is used to save the intermediate result now (more like the pentium version of fibo3).
+# So %i4 is used to load the intermediate result now (like %ecx in the pentium version of fibo3).
 # %i5 is saved and restored along only one path (like %edx in the pentium version).
 # Note that %g2 and up seem to be clobbered by the system, but they can still be used to test data flow.
 
@@ -26,12 +26,13 @@ fib:
 	st		%i5, [%fp+72]	! Save %i5 for no good reason
 	call	fib, 0
 	 nop
-	mov	%o0, %i4			! Save intermediate result
+	st	%o0, [%fp-16]		! Save intermediate result
 	ld	[%fp+68], %g1
 	add	%g1, -2, %o0
 	call	fib, 0
 	 nop
 	ld		[%fp+72], %i5	! Restore %i5
+	ld		[%fp-16], %i4	! Assign to %i4
 	add	%i4, %o0, %l0		! Use intermediate result
 	st	%l0, [%fp-20]
 	b	.LL1
