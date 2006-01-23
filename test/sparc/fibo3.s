2e8d1c08e250e639ed9d742965d801a8ae02f40d
emmerik <emmerik>
2006-01-23 00:29:17 +0000

    Changed the sparc fibo3 to use registers %i4 and %i5, which are not callee save, to work more like the pentium version

--- test/sparc/fibo3.s
+++ test/sparc/fibo3.s
@@ -1,5 +1,9 @@
 # This version is compiled with gcc version 4.0.0, no optimisation
-# It has been changed to assign to o4 and o5, and use g4, to test removal of unused returns
+# Can't use %o4 and %o5 to save the intermediate result, since the save and restore instructions effectively save and
+# restore them via the %i4 and %i5 registers (since they are not overwritten; a bit like callee save registers).
+# So %i4 is used to save the intermediate result now (more like the pentium version of fibo3).
+# %i5 is saved and restored along only one path (like %edx in the pentium version).
+# Note that %g2 and up seem to be clobbered by the system, but they can still be used to test data flow.
 
 	.file	"fibo.c"
 	.section	".text"
@@ -19,16 +23,16 @@ fib:
 	ld	[%fp+68], %g1
 	add	%g1, -1, %g1
 	mov	%g1, %o0
-	st		%g3, [%fp+72]	! Save %g4 for no good reason
+	st		%i5, [%fp+72]	! Save %i5 for no good reason
 	call	fib, 0
 	 nop
-	mov	%o0, %o4			! Save intermediate result
+	mov	%o0, %i4			! Save intermediate result
 	ld	[%fp+68], %g1
 	add	%g1, -2, %o0
 	call	fib, 0
 	 nop
-	ld		[%fp+72], %o5	! Assign to %o5
-	add	%o4, %o0, %l0		! Use intermediate result
+	ld		[%fp+72], %i5	! Restore %i5
+	add	%i4, %o0, %l0		! Use intermediate result
 	st	%l0, [%fp-20]
 	b	.LL1
 	 nop
@@ -63,25 +67,22 @@ main:
 	or	%g1, %lo(.LLC0), %o0
 	call	printf, 0
 	 nop
-	add	%fp, -20, %o5
+	add	%fp, -20, %o1
 	sethi	%hi(.LLC1), %g1
 	or	%g1, %lo(.LLC1), %o0
-	mov	%o5, %o1
 	call	scanf, 0
-	 nop
+	nop
 	ld	[%fp-20], %o0
 	call	fib, 0
 	 nop
-	mov	%o0, %g1
-	st	%g1, [%fp-24]
+	st	%o0, [%fp-24]
 	sethi	%hi(.LLC2), %g1
 	or	%g1, %lo(.LLC2), %o0
 	ld	[%fp-20], %o1
 	ld	[%fp-24], %o2
 	call	printf, 0
 	 nop
-	mov	0, %g1
-	mov	%g1, %i0
+	mov	0, %i0
 	ret
 	restore
 	.size	main, .-main
