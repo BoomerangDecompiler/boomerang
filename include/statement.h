a5e619f04bdfae9aebca4754c1f0d7288c5babb4
emmerik <emmerik>
2006-02-26 22:26:35 +0000

    Added a default constructor for the PhiAssign struct

--- include/statement.h
+++ include/statement.h
@@ -547,6 +547,9 @@ virtual	void		regReplace(UserProc* proc);
 // The below could almost be a RefExp. But we could not at one stage #include exp.h as part of statement,h; that's since
 // changed so it is now possible, and arguably desirable.  However, it's convenient to have these members public
 struct PhiInfo {
+		// A default constructor is required because CFG changes (?) can cause access to elements of the vector that
+		// are beyond the current end, creating gaps which have to be initialised to zeroes so that they can be skipped
+		PhiInfo() : def(0), e(0) {}
 		Statement*	def;		// The defining statement
 		Exp*		e;			// The expression for the thing being defined (never subscripted)
 };
