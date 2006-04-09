3bdb3d3839c5218753a4c73fd00600f4dac19fe1
emmerik <emmerik>
2006-04-09 08:50:31 +0000

    Oops - a pattern was relying on the ordering of statements for a push instruction, recently changed (fixes a segfault in "make test")

--- frontend/pentiumfrontend.cpp
+++ frontend/pentiumfrontend.cpp
@@ -680,13 +680,14 @@ ADDRESS PentiumFrontEnd::getMainEntryPoint(bool& gotMain) {
 				return cs->getFixedDest();
 			}
 			if (pBF->SymbolByAddress(dest) &&
-				strcmp(pBF->SymbolByAddress(dest), "__libc_start_main") == 0) {
+					strcmp(pBF->SymbolByAddress(dest), "__libc_start_main") == 0) {
 				// This is a gcc 3 pattern. The first parameter will be a pointer to main.
 				// Assume it's the 5 byte push immediately preceeding this instruction
+				// Note: the RTL changed recently from esp = esp-4; m[esp] = K tp m[esp-4] = K; esp = esp-4
 				inst = decodeInstruction(addr-5);
 				assert(inst.valid);
 				assert(inst.rtl->getNumStmt() == 2);
-				Assign* a = (Assign*) inst.rtl->elementAt(1);
+				Assign* a = (Assign*) inst.rtl->elementAt(0);		// Get m[esp-4] = K
 				Exp* rhs = a->getRight();
 				assert(rhs->isIntConst());
 				gotMain = true;
