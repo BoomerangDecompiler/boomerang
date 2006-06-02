5e4b103521bc3dc8e8dfd90a31cd2d2f63f38f46
emmerik <emmerik>
2006-06-02 23:15:27 +0000

    Trent's big changes deserve a new date

--- boomerang.cpp
+++ boomerang.cpp
@@ -11,7 +11,7 @@
  * 28 Jan 05 - G. Krol: Separated -h output into sections and neatened
 */
 
-#define VERSION "alpha 0.2 09/Apr/2006"
+#define VERSION "alpha 0.2 29/May/2006"
 
 #include <iostream>
 #include <fstream>
