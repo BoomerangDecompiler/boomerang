e24285c4c11419edeccd213d3aa445f11f31cb93
Artur K <nemerle5@gmail.com>
2013-01-27 23:12:21 +0100

    fixed diff commandline to actually recurse into directories

--- full_regression.sh
+++ full_regression.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #./test_use_all.sh
-./regression_tester.rb ./out/boomerang; diff -wB tests/baseline/ tests/outputs/
+./regression_tester.rb ./out/boomerang; diff -rwB tests/baseline/ tests/outputs/
