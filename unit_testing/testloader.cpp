262e4cbe0b59fc2fc74b1e43f8a885b6c19a2217
James Mitchell <git@jmitchell.id.au>
2014-05-31 23:30:33 +1000

    Commenting out protobuf includes and namespace usage, might be worth completely removing these until protobuf is being used.

--- unit_testing/testloader.cpp
+++ unit_testing/testloader.cpp
@@ -1,9 +1,9 @@
-#include <google/protobuf/text_format.h>
+/*#include <google/protobuf/text_format.h>
 #include <google/protobuf/message.h>
 #include <google/protobuf/io/zero_copy_stream.h>
-#include <google/protobuf/io/zero_copy_stream_impl_lite.h>
+#include <google/protobuf/io/zero_copy_stream_impl_lite.h>*/
 #include "testloader.h"
-using namespace google::protobuf;
+//using namespace google::protobuf;
 
 TestLoader::TestLoader() {}
 bool TestLoader::readFromString(const char *data, CodeBlock &tgt) {
