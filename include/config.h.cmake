aae02639e6bb32b247270dfbea9035544f583269
nemerle <nemerle5@gmail.com>
2014-06-12 19:44:28 +0200

    Clean-up root CMakeLists.txt, removed all unused checks and config.h.cmake defines

--- include/config.h.cmake
+++ include/config.h.cmake
@@ -1,42 +1,6 @@
 /* include/config.h.in.  Generated from cmake.  */
 #cmakedefine VERSION "@VERSION@"
 
-/* Define to 1 if you have the <inttypes.h> header file. */
-#cmakedefine HAVE_INTTYPES_H @HAVE_INTTYPES_H@
-
-/* Define to 1 if you have the `cppunit' library (-lcppunit). */
-#cmakedefine HAVE_LIBCPPUNIT @HAVE_LIBCPPUNIT@
-
-/* Define to 1 if you have the `gc' library (-lgc). */
-#cmakedefine HAVE_LIBGC @HAVE_LIBGC@
-
-/* Define to 1 if you have the <memory.h> header file. */
-#cmakedefine HAVE_MEMORY_H @HAVE_MEMORY_H@
-
-/* Define to 1 if you have the <stdint.h> header file. */
-#cmakedefine HAVE_STDINT_H @HAVE_STDINT_H@
-
-/* Define to 1 if you have the <stdlib.h> header file. */
-#cmakedefine HAVE_STDLIB_H @HAVE_STDLIB_H@
-
-/* Define to 1 if you have the <strings.h> header file. */
-#cmakedefine HAVE_STRINGS_H @HAVE_STRINGS_H@
-
-/* Define to 1 if you have the <string.h> header file. */
-#cmakedefine HAVE_STRING_H @HAVE_STRING_H@
-
-/* Define to 1 if you have the <sys/stat.h> header file. */
-#cmakedefine HAVE_SYS_STAT_H @HAVE_SYS_STAT_H@
-
-/* Define to 1 if you have the <sys/types.h> header file. */
-#cmakedefine HAVE_SYS_TYPES_H @HAVE_SYS_TYPES_H@
-
-/* Define to 1 if you have the <unistd.h> header file. */
-#cmakedefine HAVE_UNISTD_H @HAVE_UNISTD_H@
-
-/* Non zero if host machine is Mac OS X */
-#cmakedefine HOST_OSX @HOST_OSX@
-
 /* Define to the address where bug reports for this package should be sent. */
 #cmakedefine PACKAGE_BUGREPORT @PACKAGE_BUGREPORT@
 
@@ -52,35 +16,9 @@
 /* Define to the version of this package. */
 #cmakedefine PACKAGE_VERSION @PACKAGE_VERSION@
 
-/* The size of a `char', as computed by sizeof. */
-#cmakedefine SIZEOF_CHAR @SIZEOF_CHAR@
-
-/* The size of a `double', as computed by sizeof. */
-#cmakedefine SIZEOF_DOUBLE @SIZEOF_DOUBLE@
-
-/* The size of a `float', as computed by sizeof. */
-#cmakedefine SIZEOF_FLOAT @SIZEOF_FLOAT@
-
-/* The size of a `int', as computed by sizeof. */
-#cmakedefine SIZEOF_INT @SIZEOF_INT@
-
-/* The size of a `int *', as computed by sizeof. */
-#cmakedefine SIZEOF_INT_P @SIZEOF_INT_P@
-
-/* The size of a `long', as computed by sizeof. */
-#cmakedefine SIZEOF_LONG @SIZEOF_LONG@
-
-/* The size of a `long long', as computed by sizeof. */
-#cmakedefine SIZEOF_LONG_LONG @SIZEOF_LONG_LONG@
-
-/* The size of a `short', as computed by sizeof. */
-#cmakedefine SIZEOF_SHORT @SIZEOF_SHORT@
-
 /* Define to 1 if your processor stores words with the most significant byte
    first (like Motorola and SPARC, unlike Intel and VAX). */
 #cmakedefine WORDS_BIGENDIAN @WORDS_BIGENDIAN@
-
-
 
 // Defines to control experimental features
 #define USE_DOMINANCE_NUMS 1 // Set true to store a statement number that has dominance properties
