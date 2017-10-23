12360f51766f11010cbf4b3c13ba3626c9b78c03
ceeac <ceeac@users.noreply.github.com>
2017-10-23 13:51:01 +0200

    Improve error message when disassembled instruction does not match instruction in RTL dictionary.

--- src/boomerang/db/ssl/RTLInstDict.cpp
+++ src/boomerang/db/ssl/RTLInstDict.cpp
@@ -274,12 +274,14 @@ std::pair<QString, unsigned> RTLInstDict::getSignature(const char *name)
 
     if (it == idict.end()) {
         LOG_ERROR("No entry for '%1' in RTL dictionary", name);
-        it = idict.find("NOP"); // At least, don't cause segfault
+        it = idict.find("NOP");
+		if (it == idict.end()) {
+            LOG_ERROR("No entry for 'NOP' in RTL dictionary");
+			return { hlpr, 0 }; // At least, don't cause segfault
+		}
     }
 
-    return {
-               hlpr, (it->second).m_params.size()
-    };
+    return { hlpr, (it->second).m_params.size() };
 }
 
 
