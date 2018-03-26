3232dfb1abb5105a336108b0f963ce5beb0b3467
ceeac <ceeac@users.noreply.github.com>
2018-03-26 12:17:43 +0200

    Fix: Do not check if Frontend is valid at load time, some instruction sets do not have a Frontend

--- src/boomerang/core/Project.cpp
+++ src/boomerang/core/Project.cpp
@@ -155,11 +155,7 @@ Prog *Project::createProg(BinaryFile* file, const QString& name)
     m_prog.reset(new Prog(name, file));
     m_fe.reset(IFrontEnd::instantiate(getLoadedBinaryFile(), getProg()));
 
-    if (!m_fe) {
-        LOG_ERROR("Cannot instantiate frontend!");
-        m_prog.reset();
-        return nullptr;
-    }
+    // Cannot check here if  Frontend is valid, since e.g. Palm binaries do not have a frontend.
 
     m_prog->setFrontEnd(m_fe.get());
     return m_prog.get();
