c8b6a95dfb8297d27826f07d515d5ee79066ce23
ceeac <ceeac@users.noreply.github.com>
2018-03-27 09:20:32 +0200

    Remove unused ConnectionGraphTest::initTestCase

--- tests/unit-tests/boomerang/util/ConnectionGraphTest.h
+++ tests/unit-tests/boomerang/util/ConnectionGraphTest.h
@@ -19,9 +19,6 @@ public:
     Q_OBJECT
 
 private slots:
-    /// Set up anything needed before all tests
-    void initTestCase();
-
     void testAdd();
     void testConnect();
     void testCount();
