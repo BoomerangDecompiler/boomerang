f29add720d70cf88fc3ff0a64b62471614868d19
ceeac <ceeac@users.noreply.github.com>
2018-08-08 08:58:33 +0200

    Clean up BoomerangAPI.h

--- src/boomerang/core/BoomerangAPI.h
+++ src/boomerang/core/BoomerangAPI.h
@@ -12,13 +12,13 @@
 
 #ifndef BOOMERANG_API
 #  ifdef _WIN32
-#     if defined(BOOMERANG_BUILD_SHARED) /* build dll */
-#         define BOOMERANG_API __declspec(dllexport)
-#     elif !defined(BOOMERANG_BUILD_STATIC) /* use dll */
-#         define BOOMERANG_API __declspec(dllimport)
-#     else /* static library */
-#         define BOOMERANG_API
-#     endif
+#    if defined(BOOMERANG_BUILD_SHARED)     /* build dll */
+#      define BOOMERANG_API __declspec(dllexport)
+#    elif !defined(BOOMERANG_BUILD_STATIC)  /* use dll */
+#      define BOOMERANG_API __declspec(dllimport)
+#    else                                   /* static library */
+#      define BOOMERANG_API
+#    endif
 #  else
 #    define BOOMERANG_API __attribute__((visibility("default")))
 #  endif
