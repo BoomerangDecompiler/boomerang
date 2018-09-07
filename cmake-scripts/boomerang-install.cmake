43d501228b114a027afabbc80060ae53238bb871
ceeac <ceeac@users.noreply.github.com>
2018-09-07 20:44:08 +0200

    Fix: Do not install non-existent directory.
    Fixes ceeac/boomerang#2.

--- cmake-scripts/boomerang-install.cmake
+++ cmake-scripts/boomerang-install.cmake
@@ -10,7 +10,6 @@
 # always install those
 install(DIRECTORY "${CMAKE_SOURCE_DIR}/data/signatures/"      DESTINATION "share/boomerang/signatures")
 install(DIRECTORY "${CMAKE_SOURCE_DIR}/data/ssl/"             DESTINATION "share/boomerang/ssl")
-install(DIRECTORY "${CMAKE_SOURCE_DIR}/data/transformations/" DESTINATION "share/boomerang/transformations")
 
 if (BOOMERANG_BUILD_GUI)
     install(DIRECTORY "${CMAKE_SOURCE_DIR}/data/images/" DESTINATION "share/boomerang/images")
