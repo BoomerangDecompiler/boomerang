1ca840db2325601ad977b5be81fff302706aaf94
ceeac <ceeac@users.noreply.github.com>
2018-10-19 17:48:50 +0200

    Disable non-deterministic regression tests

--- tests/regression-tests/expected-outputs/pentium/twoproc3/twoproc3/twoproc3.c
+++ tests/regression-tests/expected-outputs/pentium/twoproc3/twoproc3/twoproc3.c
@@ -4,13 +4,14 @@ __size32 getDevice(__size32 param1);
 /** address: 0x0804847f */
 int main(int argc, char *argv[])
 {
-    union { pciVideoRec *; __size32; } *eax; 		// r24
-    int eax_1; 		// r24{3}
+    int eax; 		// r24
+    union { pciVideoRec *; __size32; } *eax_1; 		// r24{1}
+    int eax_2; 		// r24{3}
 
-    eax = xf86GetPciVideoInfo();
-    eax = *eax;
-    eax_1 = getDevice(eax);
-    printf("%i\n", eax_1);
+    eax_1 = xf86GetPciVideoInfo();
+    eax = *eax_1;
+    eax_2 = getDevice(eax);
+    printf("%i\n", eax_2);
     return 0;
 }
 
