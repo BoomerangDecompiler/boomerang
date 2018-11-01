4bd72ebd81b4fa937a252df6748aaa0e7d09d3c8
ceeac <ceeac@users.noreply.github.com>
2018-11-01 11:36:45 +0100

    Fix Windows package generation

--- cmake-scripts/boomerang-pack.cmake
+++ cmake-scripts/boomerang-pack.cmake
@@ -19,7 +19,7 @@ endif ()
 math(EXPR BOOMERANG_PKG_BITNESS "${CMAKE_SIZEOF_VOID_P} * 8")
 
 # actual configuration
-set(CPACK_PACKAGE_NAME ${PROJECT_NAME})
+set(CPACK_PACKAGE_NAME ${BOOMERANG_PKG_NAME})
 set(CPACK_PACKAGE_VERSION "${BOOMERANG_VERSION}")
 set(CPACK_PACKAGE_VENDOR "Boomerang Developers")
 
@@ -32,10 +32,9 @@ set(CPACK_PACKAGE_FILE_NAME "${BOOMERANG_PKG_NAME}-${BOOMERANG_VERSION}-${BOOMER
 if (WIN32 AND NOT UNIX)
     # There is a bug in NSI that does not handle full unix paths properly.
     # Make sure there is at least one set of four (4) backlasshes.
-    set(CPACK_PACKAGE_INSTALL_DIRECTORY "${BOOMERANG_PKG_NAME}\\\\${BOOMERANG_VERSION}")
-    set(CPACK_PACKAGE_ICON "${CMake_SOURCE_DIR}\\\\data/images/boomerang_icon.png")
+    set(CPACK_PACKAGE_INSTALL_DIRECTORY "${BOOMERANG_PKG_NAME}\\\\${BOOMERANG_VERSION}\\\\")
     set(CPACK_NSIS_INSTALLED_ICON_NAME "bin\\\\Boomerang-cli.exe")
-    set(CPACK_NSIS_DISPLAY_NAME "${CPACK_PACKAGE_INSTALL_DIRECTORY} Boomerang Decompiler")
+    set(CPACK_NSIS_DISPLAY_NAME "Boomerang Decompiler")
     set(CPACK_NSIS_HELP_LINK "https:\\\\\\\\github.com/BoomerangDecompiler/boomerang/issues")
     set(CPACK_NSIS_URL_INFO_ABOUT "https:\\\\\\\\github.com/BoomerangDecompiler/boomerang/")
     set(CPACK_NSIS_MODIFY_PATH ON)
@@ -48,4 +47,3 @@ endif (WIN32 AND NOT UNIX)
 
 
 include(CPack)
-
