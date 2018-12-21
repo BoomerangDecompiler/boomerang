0c789099ad1395a36fee0c207b0dcbd21930ec82
ceeac <ceeac@users.noreply.github.com>
2018-12-21 13:55:28 +0100

    Clean up FloatType.{h,cpp}

--- src/boomerang/ssl/type/FloatType.h
+++ src/boomerang/ssl/type/FloatType.h
@@ -16,7 +16,8 @@
 class BOOMERANG_API FloatType : public Type
 {
 public:
-    explicit FloatType(int sz = 64);
+    explicit FloatType(int numBits = 64);
+
     FloatType(const FloatType &other) = default;
     FloatType(FloatType &&other)      = default;
 
@@ -26,27 +27,38 @@ public:
     FloatType &operator=(FloatType &&other) = default;
 
 public:
-    static std::shared_ptr<FloatType> get(int sz = 64);
-
-    virtual SharedType clone() const override;
-
+    /// \copydoc Type::operator==
     virtual bool operator==(const Type &other) const override;
 
+    /// \copydoc Type::operator<
     virtual bool operator<(const Type &other) const override;
 
+public:
+    static std::shared_ptr<FloatType> get(int sz = 64);
+
+    /// \copydoc Type::clone
+    virtual SharedType clone() const override;
+
+public:
+    /// \copydoc Type::getSize
     virtual size_t getSize() const override;
 
-    virtual void setSize(size_t sz) override { size = sz; }
+    /// \copydoc Type::setSize
+    virtual void setSize(size_t sz) override;
 
+    /// \copydoc Type::getCtype
     virtual QString getCtype(bool final = false) const override;
 
+    /// \copydoc Type::getTempName
     virtual QString getTempName() const override;
 
     /// \copydoc Type::meetWith
     virtual SharedType meetWith(SharedType other, bool &changed, bool useHighestPtr) const override;
 
+protected:
+    /// \copydoc Type::isCompatible
     virtual bool isCompatible(const Type &other, bool all) const override;
 
 private:
-    size_t size; // Size in bits, e.g. 64
+    size_t m_size; ///< Size in bits, e.g. 64
 };
