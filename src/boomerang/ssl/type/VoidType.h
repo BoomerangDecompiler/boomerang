9a2f9b7b698d189a2b01ab747935c699ab4de56c
ceeac <ceeac@users.noreply.github.com>
2018-12-21 15:08:53 +0100

    Clean up VoidType.{h,cpp}

--- src/boomerang/ssl/type/VoidType.h
+++ src/boomerang/ssl/type/VoidType.h
@@ -17,6 +17,7 @@ class BOOMERANG_API VoidType : public Type
 {
 public:
     VoidType();
+
     VoidType(const VoidType &other) = default;
     VoidType(VoidType &&other)      = default;
 
@@ -26,24 +27,29 @@ public:
     VoidType &operator=(VoidType &&other) = default;
 
 public:
-    virtual SharedType clone() const override;
-
     static std::shared_ptr<VoidType> get() { return std::make_shared<VoidType>(); }
 
+    /// \copydoc Type::clone
+    virtual SharedType clone() const override;
+
+public:
+    /// \copydoc Type::operator==
     virtual bool operator==(const Type &other) const override;
 
+    /// \copydoc Type::operator<
     virtual bool operator<(const Type &other) const override;
 
+public:
+    /// \copydoc Type::getSize
     virtual size_t getSize() const override;
 
-    /**
-     * Return a string representing this type
-     * \param        final if true, this is final output
-     * \returns      Pointer to a constant string of char
-     */
+    /// \copydoc Type::getCtype
     virtual QString getCtype(bool final = false) const override;
 
     /// \copydoc Type::meetWith
     virtual SharedType meetWith(SharedType other, bool &changed, bool useHighestPtr) const override;
+
+protected:
+    /// \copydoc Type::isCompatible
     virtual bool isCompatible(const Type &other, bool all) const override;
 };
