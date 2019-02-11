05b36e3092b01e82af5bec530f866f0986716094
ceeac <ceeac@users.noreply.github.com>
2019-02-11 13:12:48 +0100

    Remove BCCTR_LONG define

--- cmake-scripts/boomerang-configure.cmake
+++ cmake-scripts/boomerang-configure.cmake
@@ -79,7 +79,6 @@ endif ()
 
 add_definitions(-DDEBUG=0)
 
-add_definitions(-DBCCTR_LONG=0)
 add_definitions(-DYYMAXDEPTH=10000)
 add_definitions(-DSYMS_IN_BACK_END=0)
 add_definitions(-DDEBUG_SPLIT_FOR_BRANCH=0)
