ca399d819a0d1709954568e19bc4405e346c0eac
ceeac <ceeac@users.noreply.github.com>
2019-03-19 14:31:45 +0100

    Add tests for addcc

--- tests/unit-tests/boomerang-plugins/decoder/sparc/SPARCDecoderTest.cpp
+++ tests/unit-tests/boomerang-plugins/decoder/sparc/SPARCDecoderTest.cpp
@@ -65,11 +65,47 @@ void SPARCDecoderTest::testInstructions_data()
 
     TEST_DECODE("add %g3, %g1, %g2", "\x84\x00\xc0\x01", ICLASS::NCT,
                 "0x00001000    0 *32* tmp := r3\n"
-                "              0 *32* r2 := r3 + r1\n");
+                "              0 *32* r2 := r3 + r1\n"
+    );
 
     TEST_DECODE("add %g3, 1, %g2", "\x84\x00\xe0\x01", ICLASS::NCT,
                 "0x00001000    0 *32* tmp := r3\n"
-                "              0 *32* r2 := r3 + 1\n");
+                "              0 *32* r2 := r3 + 1\n"
+    );
+
+    TEST_DECODE("addcc %g3, %g1, %g2", "\x84\x80\xc0\x01", ICLASS::NCT,
+                "0x00001000    0 *32* tmp := r3\n"
+                "              0 *32* r2 := r3 + r1\n"
+                "              0 *v* %flags := ADDFLAGS( tmp, r1, r2 )\n"
+    );
+
+    TEST_DECODE("addcc %g3, 1, %g2", "\x84\x80\xe0\x01", ICLASS::NCT,
+                "0x00001000    0 *32* tmp := r3\n"
+                "              0 *32* r2 := r3 + 1\n"
+                "              0 *v* %flags := ADDFLAGS( tmp, 1, r2 )\n"
+    );
+
+    TEST_DECODE("addx %g3, %g1, %g2", "\x84\x40\xc0\x01", ICLASS::NCT,
+                "0x00001000    0 *32* tmp := r3\n"
+                "              0 *32* r2 := r3 + (r1 + zfill(1, 32, %CF))\n"
+    );
+
+    TEST_DECODE("addx %g3, 1, %g2", "\x84\x40\xe0\x01", ICLASS::NCT,
+                "0x00001000    0 *32* tmp := r3\n"
+                "              0 *32* r2 := (r3 + zfill(1, 32, %CF)) + 1\n"
+    );
+
+    TEST_DECODE("addxcc %g3, %g1, %g2", "\x84\xc0\xc0\x01", ICLASS::NCT,
+                "0x00001000    0 *32* tmp := r3\n"
+                "              0 *32* r2 := r3 + (r1 + zfill(1, 32, %CF))\n"
+                "              0 *v* %flags := ADDFLAGS( tmp, r1, r2 )\n"
+    );
+
+    TEST_DECODE("addxcc %g3, 1, %g2", "\x84\xc0\xe0\x01", ICLASS::NCT,
+                "0x00001000    0 *32* tmp := r3\n"
+                "              0 *32* r2 := (r3 + zfill(1, 32, %CF)) + 1\n"
+                "              0 *v* %flags := ADDFLAGS( tmp, 1, r2 )\n"
+    );
 }
 
 
