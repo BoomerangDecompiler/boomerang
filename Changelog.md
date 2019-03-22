046150bc3d5a702ccb62dff11ce3ea988ef6cdcd
ceeac <ceeac@users.noreply.github.com>
2019-03-22 15:50:54 +0100

    Fix crashes when decoding unrecognized floating point branches on SPARC

--- Changelog.md
+++ Changelog.md
@@ -1,6 +1,7 @@
 v0.5.0 (in development)
 -----------------------
 - Fixed: Crashes when reading malformed SSL specification files.
+- Fixed: Crashes when decoding unrecognized floating poing branches on SPARC.
 - Fixed: Crash when decoding instructions with multiple instruction prefixes in some cases.
 - Fixed: Crash when decompiling x86 binaries that contain specific variants of the JP or JNP instructions.
 - Fixed: Crash when decompiling x86 binaries that contain functions where the first instruction is BSF or BSR.
--- src/boomerang-plugins/decoder/sparc/SPARCDecoder.cpp
+++ src/boomerang-plugins/decoder/sparc/SPARCDecoder.cpp
@@ -125,7 +125,10 @@ std::unique_ptr<RTL> SPARCDecoder::createBranchRTL(const char *insnName, Address
             // Else it's FBN!
             break;
 
-        default: LOG_WARN("Unknown float branch '%1'", insnName); stmts.reset();
+        default:
+            LOG_ERROR("Unknown float branch '%1'", insnName);
+            stmts->clear();
+            break;
         }
 
         return stmts;
@@ -1803,7 +1806,8 @@ bool SPARCDecoder::decodeInstruction(Address pc, ptrdiff_t delta, DecodeResult &
 
             GotoStatement *jump = nullptr;
 
-            if ((strcmp(name, "BA") == 0) || (strcmp(name, "BN") == 0)) {
+            if ((strcmp(name, "BA") == 0) || (strcmp(name, "BN") == 0) ||
+                (strcmp(name, "FBA") == 0) || (strcmp(name, "FBN") == 0)) {
                 jump = new GotoStatement;
                 inst.rtl->append(jump);
             }
@@ -1813,7 +1817,10 @@ bool SPARCDecoder::decodeInstruction(Address pc, ptrdiff_t delta, DecodeResult &
             }
             else {
                 inst.rtl = createBranchRTL(name, pc, std::move(inst.rtl));
-                jump     = static_cast<BranchStatement *>(inst.rtl->back());
+
+                if (!inst.rtl->empty() && inst.rtl->back()->isBranch()) {
+                    jump     = static_cast<BranchStatement *>(inst.rtl->back());
+                }
             }
 
             // The class of this instruction depends on whether or not it is one of the
@@ -1823,15 +1830,19 @@ bool SPARCDecoder::decodeInstruction(Address pc, ptrdiff_t delta, DecodeResult &
 
             inst.type = SCD;
 
-            if ((strcmp(name, "BA") == 0) || (strcmp(name, "BVC") == 0)) {
+            if ((strcmp(name, "BA") == 0) || (strcmp(name, "BVC") == 0) ||
+                (strcmp(name, "FBA") == 0)) {
                 inst.type = SD;
             }
-
-            if ((strcmp(name, "BN") == 0) || (strcmp(name, "BVS") == 0)) {
+            else if ((strcmp(name, "BN") == 0) || (strcmp(name, "BVS") == 0) ||
+                (strcmp(name, "FBN") == 0)) {
                 inst.type = NCT;
             }
 
-            jump->setDest(Address((tgt - delta).value()));
+            if (jump) {
+                jump->setDest(Address((tgt - delta).value()));
+            }
+
             SHOW_ASM(name << " " << tgt - delta)
 
             DEBUG_STMTS(inst);
@@ -1869,10 +1880,10 @@ bool SPARCDecoder::decodeInstruction(Address pc, ptrdiff_t delta, DecodeResult &
             // 'unconditional' conditional branches
 
             // "BA,A" or "BN,A"
-            if (strcmp(name, "BA,a") == 0) {
+            if (strcmp(name, "BA,a") == 0 || strcmp(name, "FBA,a") == 0) {
                 inst.type = SU;
             }
-            else if (strcmp(name, "BN,a") == 0) {
+            else if (strcmp(name, "BN,a") == 0 || strcmp(name, "FBN,a") == 0) {
                 inst.type = SKIP;
             }
             else {
@@ -1892,9 +1903,11 @@ bool SPARCDecoder::decodeInstruction(Address pc, ptrdiff_t delta, DecodeResult &
                 jump     = static_cast<GotoStatement *>(inst.rtl->back());
             }
 
-            jump->setDest(Address((tgt - delta).value()));
-            SHOW_ASM(name << " " << tgt - delta)
+            if (jump) {
+                jump->setDest(Address((tgt - delta).value()));
+            }
 
+            SHOW_ASM(name << " " << tgt - delta)
             DEBUG_STMTS(inst);
         }
         goto MATCH_finished_d;
