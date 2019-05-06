fdc58d99410dab09086e775f4811c199c017b1fc
ceeac <ceeac@users.noreply.github.com>
2019-05-06 20:43:37 +0200

    Fix missing comparison for function pointer constants

--- src/boomerang/ssl/exp/Const.cpp
+++ src/boomerang/ssl/exp/Const.cpp
@@ -299,6 +299,12 @@ bool Const::operator==(const Exp &other) const
     case opLongConst: return getLong() == otherConst.getLong();
     case opFltConst: return getFlt() == otherConst.getFlt();
     case opStrConst: return getStr() == otherConst.getStr();
+    case opFuncConst: {
+        const Function *const *myData = std::get_if<Function *>(&m_value);
+        const Function *const *otherData = std::get_if<Function *>(&otherConst.m_value);
+
+        return myData && otherData && *myData == *otherData;
+    }
     default: LOG_FATAL("Invalid operator %1", operToString(m_oper));
     }
 
