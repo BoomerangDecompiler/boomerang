77331fbd0b1df4c3ba3f706ba0fa16a92a8620ea
ceeac <ceeac@users.noreply.github.com>
2019-05-07 11:37:39 +0200

    Use static_cast instead of dynamic_cast for speed

--- src/boomerang/visitor/expmodifier/CallBypasser.cpp
+++ src/boomerang/visitor/expmodifier/CallBypasser.cpp
@@ -33,9 +33,10 @@ SharedExp CallBypasser::postModify(const std::shared_ptr<RefExp> &exp)
     // Note: r (the pointer) will always == ret (also the pointer) here, so the below is safe and
     // avoids a cast
     Statement *def      = exp->getDef();
-    CallStatement *call = dynamic_cast<CallStatement *>(def);
 
-    if (call) {
+    if (def && def->isCall()) {
+        CallStatement *call = static_cast<CallStatement *>(def);
+
         assert(std::dynamic_pointer_cast<RefExp>(ret));
         bool ch;
         ret = call->bypassRef(ret->access<RefExp>(), ch);
