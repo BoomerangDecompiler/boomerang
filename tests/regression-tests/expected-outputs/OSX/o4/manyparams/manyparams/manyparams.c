03d03e511f94cc9dc6db37683e14d4fe8e22cf39
ceeac <ceeac@users.noreply.github.com>
2019-09-09 15:00:33 +0200

    Fix regression tests

--- tests/regression-tests/expected-outputs/OSX/o4/manyparams/manyparams/manyparams.c
+++ tests/regression-tests/expected-outputs/OSX/o4/manyparams/manyparams/manyparams.c
@@ -6,13 +6,13 @@ union { double; __size32; } global_0x00001ff4;
 /** address: 0x00001ca8 */
 int main(int argc, char *argv[])
 {
-    int g5; 		// r5
-    int g8; 		// r8
+    double g5_1; 		// r5{3}
+    long long g8_1; 		// r8{4}
     double local0; 		// m[g1 - 120]
 
-    g5 = *0x1fe8;
-    g8 = *0x1ff0;
-    printf("Many parameters: %d, %.1f, %d, %.1f, %d, %.1f, %d, %.1f\n", 1, g5, global_0x00001fec, 2.8025969e-45, g8, global_0x00001ff4, 3, local0);
+    g5_1 = *0x1fe8;
+    g8_1 = *0x1ff0;
+    printf("Many parameters: %d, %.1f, %d, %.1f, %d, %.1f, %d, %.1f\n", 1, g5_1, global_0x00001fec, 2.8025969e-45, g8_1, global_0x00001ff4, 3, local0);
     return 0;
 }
 
