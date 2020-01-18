1bb100dff767b06470e4dac5fb8c2527ca29d0d8
ceeac <ceeac@users.noreply.github.com>
2020-01-18 14:04:22 +0100

    Add PhiAssignTest

--- tests/unit-tests/boomerang/ssl/statements/StatementTest.cpp
+++ tests/unit-tests/boomerang/ssl/statements/StatementTest.cpp
@@ -48,27 +48,7 @@
 
 void StatementTest::testClone()
 {
-    // PhiAssign
-    {
-        std::shared_ptr<PhiAssign> phi(new PhiAssign(IntegerType::get(32, Sign::Signed), Location::regOf(REG_X86_EAX)));
-        SharedStmt clone = phi->clone();
 
-        QVERIFY(&(*clone) != &(*phi));
-        QVERIFY(clone->isPhi());
-        QVERIFY(clone->getID() != (uint32)-1);
-        QVERIFY(clone->getID() != phi->getID());
-
-        std::shared_ptr<PhiAssign> phiClone = clone->as<PhiAssign>();
-        QCOMPARE(phiClone->getNumDefs(), 0);
-
-        QVERIFY(phiClone->getLeft() != nullptr);
-        QVERIFY(phiClone->getLeft() != phi->getLeft());
-        QVERIFY(*phiClone->getLeft() == *phi->getLeft());
-
-        QVERIFY(phiClone->getType() != nullptr);
-        QVERIFY(phiClone->getType() != phi->getType());
-        QVERIFY(*phiClone->getType() == *phi->getType());
-    }
 
     // ReturnStatement
     {
@@ -145,16 +125,6 @@ void StatementTest::testIsNull()
 
 void StatementTest::testGetDefinitions()
 {
-    // PhiAssign
-    {
-        LocationSet defs;
-
-        // %eax := phi()
-        std::shared_ptr<PhiAssign> phi(new PhiAssign(IntegerType::get(32, Sign::Signed), Location::regOf(REG_X86_EAX)));
-        phi->getDefinitions(defs, false);
-        QCOMPARE(defs.toString(), "r24");
-    }
-
     // ReturnStatement
     {
         Prog prog("testProg", &m_project);
@@ -183,29 +153,6 @@ void StatementTest::testGetDefinitions()
 
 void StatementTest::testDefinesLoc()
 {
-    // PhiAssign
-    {
-        // %eax := phi()
-        std::shared_ptr<PhiAssign> phi(new PhiAssign(Location::regOf(REG_X86_EAX)));
-
-        QVERIFY(phi->definesLoc(Location::regOf(REG_X86_EAX)));
-        QVERIFY(!phi->definesLoc(Location::regOf(REG_X86_ECX)));
-    }
-
-    {
-        const SharedExp def = Ternary::get(opAt,
-                                           Location::regOf(REG_X86_EAX),
-                                           Const::get(0),
-                                           Const::get(7));
-
-        // %eax@[0:7] := phi()
-        std::shared_ptr<PhiAssign> phi(new PhiAssign(def));
-
-        QVERIFY(phi->definesLoc(def));
-        QVERIFY(phi->definesLoc(Location::regOf(REG_X86_EAX)));
-        QVERIFY(!phi->definesLoc(Location::regOf(REG_X86_ECX)));
-    }
-
     // BoolAssign
     {
         // %eax := (%ecx != 0)
@@ -273,20 +220,6 @@ void StatementTest::testDefinesLoc()
 
 void StatementTest::testSearch()
 {
-    // PhiAssign
-    {
-        std::shared_ptr<PhiAssign> phi(new PhiAssign(Location::regOf(REG_X86_EAX)));
-
-        SharedExp result;
-        QVERIFY(!phi->search(*Location::regOf(REG_X86_ECX), result));
-        QVERIFY(phi->search(*Location::regOf(REG_X86_EAX), result));
-        QVERIFY(result != nullptr);
-        QCOMPARE(*result, *Location::regOf(REG_X86_EAX));
-    }
-
-    // TODO: phi with arguments
-
-
     // ReturnStatement
     {
         // TODO verify it only searches the returns and not the modifieds etc.
@@ -306,22 +239,6 @@ void StatementTest::testSearch()
 
 void StatementTest::testSearchAll()
 {
-    // PhiAssign
-    {
-        SharedExp eax = Location::regOf(REG_X86_EAX);
-        std::shared_ptr<PhiAssign> phi(new PhiAssign(eax));
-
-        std::list<SharedExp> result;
-        QVERIFY(!phi->searchAll(*Location::regOf(REG_X86_ECX), result));
-        QVERIFY(result.empty());
-
-        QVERIFY(phi->searchAll(*eax, result));
-        QCOMPARE(result, { eax });
-    }
-
-    // TODO: phi with arguments
-
-
     // ReturnStatement
     {
         SharedExp eax = Location::regOf(REG_X86_EAX);
@@ -344,18 +261,6 @@ void StatementTest::testSearchAll()
 
 void StatementTest::testSearchAndReplace()
 {
-    // PhiAssign
-    {
-        std::shared_ptr<PhiAssign> phi(new PhiAssign(Location::regOf(REG_X86_EAX)));
-        std::shared_ptr<PhiAssign> phiClone = phi->clone()->as<PhiAssign>();
-        phiClone->setLeft(Location::regOf(REG_X86_ECX));
-
-        QVERIFY(phi->searchAndReplace(*Location::regOf(REG_X86_EAX), Location::regOf(REG_X86_ECX)));
-        QCOMPARE(phi->toString(), phiClone->toString());
-    }
-
-    // TODO Phi with arguments
-
 
     // ReturnStatement
     // TODO
