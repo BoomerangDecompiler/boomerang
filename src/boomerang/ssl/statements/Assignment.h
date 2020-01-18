d28a6c1097116e123c3101f5fe3fac8fb0a82a70
ceeac <ceeac@users.noreply.github.com>
2020-01-18 10:04:42 +0100

    Remove Assignment::getLeftRef

--- src/boomerang/ssl/statements/Assignment.h
+++ src/boomerang/ssl/statements/Assignment.h
@@ -61,8 +61,6 @@ public:
     /// \returns the expression defining the left hand side of the assignment
     SharedExp getLeft() const;
 
-    SharedExp &getLeftRef() { return m_lhs; }
-
     /// Update the left hand side of the assignment
     void setLeft(SharedExp e);
 
