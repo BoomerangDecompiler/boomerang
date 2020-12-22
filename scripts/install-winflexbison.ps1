c7ff0d8e950f46247947a702f38a4732e706ac1f
ceeac <ceeac@users.noreply.github.com>
2020-12-22 15:26:02 +0100

    Update winflexbison to v2.5.23

--- scripts/install-winflexbison.ps1
+++ scripts/install-winflexbison.ps1
@@ -2,13 +2,13 @@
 # Install flex + bison via winflexbison
 if (!(Test-Path winflexbison.zip)) {
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    if (Invoke-WebRequest -Uri https://github.com/lexxmark/winflexbison/releases/download/v2.5.18/win_flex_bison-2.5.18.zip -OutFile winflexbison.zip) {
+    if (Invoke-WebRequest -Uri https://github.com/lexxmark/winflexbison/releases/download/v2.5.23/win_flex_bison-2.5.23.zip -OutFile winflexbison.zip) {
         Write-Output "Could not download winflexbison"
         exit 1
     }
 }
 
-$expectedhash = "095CF65CB3F12EE5888022F93109ACBE6264E5F18F6FFCE0BDA77FEB31B65BD8"
+$expectedhash = "6AA5C8EA662DA1550020A5804C28BE63FFAA53486DA9F6842E24C379EC422DFC"
 $actualhash = (Get-FileHash -Algorithm "SHA256" winflexbison.zip).hash
 
 if ($actualhash -ne $expectedhash) {
